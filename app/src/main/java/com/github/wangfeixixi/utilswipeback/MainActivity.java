548c9217a873198d1244bd591d9d4f16b12b75f9
xuanyuanxixi <1020301980@qq.com>
2017-12-28 17:30:27 +0800

    Initial commit

---
+++ app/src/main/java/com/github/wangfeixixi/utilswipeback/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.wangfeixixi.utilswipeback;
+
+import android.os.Bundle;
+
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+public class MainActivity extends SwipeBackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
